f4239dd93f6c07bbb369975120f29f5d78bae289
carlanjoroge <carlanjoroge16@gmail.com>
2023-07-25 04:17:06 -0700

    debugging

--- main.c
+++ main.c
@@ -9,38 +9,38 @@
  */
 int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 {
-    char *line = NULL;      /* Pointer to store user input line */
-    size_t bufsize = 0;     /* Size of the buffer for getline */
-    info_t info = INFO_INIT; /* Structure to hold shell information */
-    int int_mode = 1;       /* Flag to check if running interactively */
+char *line = NULL;      /* Pointer to store user input line */
+size_t bufsize = 0;     /* Size of the buffer for getline */
+info_t info = INFO_INIT; /* Structure to hold shell information */
+int int_mode = 1;       /* Flag to check if running interactively */
 
-    while (int_mode)
-    {
-        int_mode = isatty(STDIN_FILENO);
-        if (int_mode == 1)
-        {
-            writePrompt(); /* Display the shell prompt */
-        }
+while (int_mode)
+{
+int_mode = isatty(STDIN_FILENO);
+if (int_mode == 1)
+{
+writePrompt(); /* Display the shell prompt */
+}
 
-        /* Read the input command line from the user */
-        if (my_getline(&line, &bufsize, stdin) == -1)
-        {
-            if (int_mode)
-                putchar('\n');
-            break; /* Exit the loop on end-of-file (EOF) */
-        }
+/* Read the input command line from the user */
+if (my_getline(&line, &bufsize, stdin) == -1)
+{
+if (int_mode)
+putchar('\n');
+break; /* Exit the loop on end-of-file (EOF) */
+}
 
-        /* Remove the newline character from the input */
-        line[strcspn(line, "\n")] = '\0';
+/* Remove the newline character from the input */
+line[strcspn(line, "\n")] = '\0';
 
-        /* Execute the command */
-        execute_command(&info, line);
+/* Execute the command */
+execute_command(&info, line);
 
-        /* Free allocated memory for the line */
-        free(line);
-        line = NULL;
-    }
+/* Free allocated memory for the line */
+free(line);
+line = NULL;
+}
 
-    return EXIT_SUCCESS;
+return (EXIT_SUCCESS);
 }
 
